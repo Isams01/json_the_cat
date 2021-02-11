5003bd0048cc7616f3279b78d019b87eb2703d3d
Isamu Ito <isamsito@gmail.com>
2021-02-11 17:26:23 +0000

    add edge cases for status code 200 and breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,18 @@
 const request = require('request');
+
+const endpoint = 'https://api.thecatapi.com/v1/breeds/search?name=siberian';
+
+request(endpoint, (error, response, body) => {
+  const catData = JSON.parse(body);
+  if (response.statusCode !== 200) {
+    console.log('Uh oh, something went wrong');
+    process.exit(0);
+  }
+  else if (body.length === 0) {
+    console.log('Bread not found')
+  }
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+});
+
